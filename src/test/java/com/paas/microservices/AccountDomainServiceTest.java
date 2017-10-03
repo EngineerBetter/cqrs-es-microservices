9f4347c3c3a7de4e36412a5340f75f9e9315ae21
DanielJonesEB <daniel.jones@engineerbetter.com>
2017-10-03 14:05:01 +0100

    debits are idempotent

--- src/test/java/com/paas/microservices/AccountDomainServiceTest.java
+++ src/test/java/com/paas/microservices/AccountDomainServiceTest.java
@@ -10,64 +10,78 @@ import org.junit.Test;
 public class AccountDomainServiceTest {
 	private AccountRepository repo;
 	private AccountDomainService accountService;
+	private Account account;
 
 	@Before
 	public void setup() {
 		repo = new InMemoryAccountRepository();
 		accountService = new RepositoryAccountDomainService(repo);
+		account = createAccount();
+	}
+
+	private Account createAccount() {
+		UUID transactionId = UUID.randomUUID();
+		Account created = accountService.createAccount(transactionId);
+		return created;
 	}
 
 	@Test
 	public void newAccountsHaveAZeroBalance() {
-		UUID transactionId = UUID.randomUUID();
-		Account account = accountService.createAccount(transactionId);
 		assertThat(account.balance).isEqualTo(0d);
 	}
 
 	@Test
 	public void createAccountRequestsAreIdempotent() {
 		UUID transactionId = UUID.randomUUID();
+		int accountsBefore = repo.getTotalNumberOfAccounts();
 		accountService.createAccount(transactionId);
 		accountService.createAccount(transactionId);
-		assertThat(repo.getTotalNumberOfAccounts()).isEqualTo(1);
+		assertThat(repo.getTotalNumberOfAccounts()).isEqualTo(accountsBefore + 1);
 	}
 
 	@Test
 	public void accountsAreCreditable() {
-		UUID transactionId = UUID.randomUUID();
-		Account created = accountService.createAccount(transactionId);
-		assertThat(created.balance).isEqualTo(0);
+		assertThat(account.balance).isEqualTo(0);
 
 		UUID creditTxId = UUID.randomUUID();
-		accountService.creditAccount(creditTxId, created.accountNumber, 50d);
-		double balance = accountService.getBalance(created.accountNumber);
+		accountService.creditAccount(creditTxId, account.accountNumber, 50d);
+		double balance = accountService.getBalance(account.accountNumber);
 		assertThat(balance).isEqualTo(50d);
 	}
 
 	@Test
 	public void creditsAreIdempotent() {
-		UUID transactionId = UUID.randomUUID();
-		Account created = accountService.createAccount(transactionId);
-		assertThat(created.balance).isEqualTo(0);
+		assertThat(account.balance).isEqualTo(0);
 
 		UUID creditTxId = UUID.randomUUID();
-		accountService.creditAccount(creditTxId, created.accountNumber, 50d);
-		accountService.creditAccount(creditTxId, created.accountNumber, 50d);
-		double balance = accountService.getBalance(created.accountNumber);
+		accountService.creditAccount(creditTxId, account.accountNumber, 50d);
+		accountService.creditAccount(creditTxId, account.accountNumber, 50d);
+		double balance = accountService.getBalance(account.accountNumber);
 		assertThat(balance).isEqualTo(50d);
 	}
 
 	@Test
 	public void accountsAreDebitable() {
-		UUID transactionId = UUID.randomUUID();
-		Account created = accountService.createAccount(transactionId);
-		assertThat(created.balance).isEqualTo(0);
-		accountService.creditAccount(UUID.randomUUID(), created.accountNumber, 200d);
+		assertThat(account.balance).isEqualTo(0);
+		accountService.creditAccount(UUID.randomUUID(), account.accountNumber, 200d);
 
-		accountService.debitAccount(UUID.randomUUID(), created.accountNumber, 100d);
-		accountService.debitAccount(UUID.randomUUID(), created.accountNumber, 50d);
-		double balance = accountService.getBalance(created.accountNumber);
+		accountService.debitAccount(UUID.randomUUID(), account.accountNumber, 100d);
+		accountService.debitAccount(UUID.randomUUID(), account.accountNumber, 50d);
+		double balance = accountService.getBalance(account.accountNumber);
 		assertThat(balance).isEqualTo(50);
+	}
+
+	@Test
+	public void debitsAreIdempotent() {
+		assertThat(account.balance).isEqualTo(0);
+
+		UUID creditTxID = UUID.randomUUID();
+		accountService.creditAccount(creditTxID, account.accountNumber, 100d);
+		UUID debitTxID = UUID.randomUUID();
+		accountService.debitAccount(debitTxID, account.accountNumber, 30d);
+		accountService.debitAccount(debitTxID, account.accountNumber, 30d);
+		double balance = accountService.getBalance(account.accountNumber);
+		assertThat(balance).isEqualTo(70d);
 
 	}
 }
